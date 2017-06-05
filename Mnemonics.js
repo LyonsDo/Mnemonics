fdd0fa2e6ad6d082113a501c7eaf7c174283595b
LyonsDo <LyonsDo@users.noreply.github.com>
2017-06-05 11:29:34 +0200

    Update Mnemonics.js

--- Mnemonics.js
+++ Mnemonics.js
@@ -174,22 +174,3 @@ for (i = 0; i < m_dat.length; i++) {
         google.maps.event.removeListener(boundsListener);
     });
 }
-
-
-      function downloadUrl(url, callback) {
-        var request = window.ActiveXObject ?
-            new ActiveXObject('Microsoft.XMLHTTP') :
-            new XMLHttpRequest;
-
-        request.onreadystatechange = function() {
-          if (request.readyState == 4) {
-            request.onreadystatechange = doNothing;
-            callback(request, request.status);
-          }
-        };
-
-        request.open('GET', url, true);
-        request.send(null);
-      }
-
-      function doNothing() {}
